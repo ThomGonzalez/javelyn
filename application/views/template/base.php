6aa19f1585c508b52b44662253b24ccab1221a21
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-03 17:51:17 -0500

    Hola mundo! CodeIgniter.

---
+++ application/views/template/base.php
@@ -0,0 +1,15 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Index</title>
+</head>
+<body>
+
+<div id="container">
+	<h4>Hello worl!</h4>
+</div>
+
+</body>
+</html>

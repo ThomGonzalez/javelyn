2dc656c3c5d63cf7d699313764d6fe16e765efc4
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-05 00:46:13 -0500

    Preloader.

--- application/views/template/base.php
+++ application/views/template/base.php
@@ -1,3 +1,10 @@
-<body>
+<body id="body">
 <p>Hello Javelyn</p>
+	<!-- preloader -->
+		<div id="preloader">
+			<img src="<?=base_url('images/preloader.gif')?>" alt="preloader"/>
+
+		</div>
+	<!-- end preloader -->
+
 </body>

bbbb62709dcc82b23ed40c8cff95479388fc280d
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-05 03:29:27 -0500

    Section de trabajos.

--- application/views/template/base.php
+++ application/views/template/base.php
@@ -154,4 +154,110 @@
 	</section>
     <!-- End Features -->
 
+    <!-- Our Works -->
+	<section id="works" class="works">
+		<div class="container">
+			<div class="row">
+			
+				<div class="sec-title text-center">
+					<h2>Works</h2>
+					<div class="devider"><i class="fa fa-heart-o fa-lg"></i></div>
+				</div>
+				
+				<div class="sec-sub-title text-center">
+					<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+				</div>
+				
+				<div class="work-filter wow fadeInRight animated" data-wow-duration="500ms">
+					<ul class="text-center">
+						<li><a href="#" data-filter="*" class="current">All</a></li>
+						<li><a href="#" data-filter=".branding">Branding</a></li>
+						<li><a href="#" data-filter=".web">web</a></li>
+						<li><a href="#" data-filter=".logo-design">logo design</a></li>
+						<li><a href="#" data-filter=".photography">photography</a></li>
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+		
+		<div class="project-wrapper">
+		
+			<figure class="work-item branding">
+				<img src="<?=base_url('images/works/item-1.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-1.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+			
+			<figure class="work-item web">
+				<img src="<?=base_url('images/works/item-2.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-2.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+			
+			<figure class="work-item logo-design">
+				<img src="<?=base_url('images/works/item-3.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-3.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+			
+			<figure class="work-item photography">
+				<img src="<?=base_url('images/works/item-4.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-4.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+		
+			<figure class="work-item branding">
+				<img src="<?=base_url('images/works/item-5.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-5.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+			
+			<figure class="work-item web">
+				<img src="<?=base_url('images/works/item-6.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-6.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+			
+			<figure class="work-item logo-design">
+				<img src="<?=base_url('images/works/item-7.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-7.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+			
+			<figure class="work-item photography">
+				<img src="<?=base_url('images/works/item-8.jpg')?>" alt="">
+				<figcaption class="overlay">
+					<a class="fancybox" rel="works" title="Write Your Image Caption Here" href="<?=base_url('images/works/item-8.jpg')?>"><i class="fa fa-eye fa-lg"></i></a>
+					<h4>Labore et dolore magnam</h4>
+					<p>Photography</p>
+				</figcaption>
+			</figure>
+			
+		</div>
+	</section>
+	<!-- End Our Works -->
+		
+
 </body>

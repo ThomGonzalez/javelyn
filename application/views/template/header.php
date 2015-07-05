38ebbbcee4b4cd92d463907f3aef32f5d5a6a88d
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-05 00:35:58 -0500

    Se importó recursos de bootstrap.

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -3,6 +3,18 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title>Index</title>
+	<!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- CSS -->
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/default.css" type="text/css" />
+	<!-- Twitter Bootstrap css -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap-theme.min.css" type="text/css" />
+    <!-- JS -->
+    <script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
 </head>

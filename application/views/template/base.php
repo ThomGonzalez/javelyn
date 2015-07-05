3315b3dd41a4383e9457ac6a8c86ec937eafd4c1
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-05 01:28:49 -0500

    Menú de navegación.

--- application/views/template/base.php
+++ application/views/template/base.php
@@ -1,10 +1,45 @@
 <body id="body">
-<p>Hello Javelyn</p>
+<p>Javelyn</p>
 	<!-- preloader -->
 		<div id="preloader">
 			<img src="<?=base_url('images/preloader.gif')?>" alt="preloader"/>
 
 		</div>
 	<!-- end preloader -->
+
+	 <!-- Fixed Navigation -->
+        <header id="navigation" class="navbar-fixed-top navbar">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- responsive nav button -->
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <i class="fa fa-bars fa-2x"></i>
+                    </button>
+					<!-- /responsive nav button -->
+					
+					<!-- logo -->
+                    <a class="navbar-brand" href="#body">
+						<h1 id="logo">
+							<img src="img/logo.png" alt="Brandi">
+						</h1>
+					</a>
+					<!-- /logo -->
+                </div>
+
+				<!-- main nav -->
+                <nav class="collapse navbar-collapse navbar-right" role="navigation">
+                    <ul id="nav" class="nav navbar-nav">
+                        <li class="current"><a href="#body">Inicio</a></li>
+                        <li><a href="#features">Proyecto</a></li>
+                        <li><a href="#works">Trabajos</a></li>
+                        <li><a href="#team">Equipo</a></li>
+                        <li><a href="#contact">Contacto</a></li>
+                    </ul>
+                </nav>
+				<!-- /main nav -->				
+            </div>
+        </header>
+        <!-- End Fixed Navigation -->
 
 </body>

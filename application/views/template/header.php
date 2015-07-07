3667101bda47658964c9cc37a6d0859419fa017a
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-06 23:38:10 -0500

    Nuevos recursos js.

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -33,11 +33,28 @@
     <script type="text/javascript" src="<?php echo base_url();?>js/jquery-single-page-nav.min.js"></script>
     <!-- Twitter Bootstrap -->
     <script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
-    <!-- Custom Functions -->
-    <script type="text/javascript" src="<?php echo base_url();?>js/custom.js"></script>
     <!-- jquery fancybox pack -->
     <script type="text/javascript" src="<?php echo base_url();?>js/jquery.fancybox.pack.js"></script>
     <!-- jquery isotope -->
     <script type="text/javascript" src="<?php echo base_url();?>js/jquery-isotope.js"></script>
+    <!-- jquery parallax -->
+    <script type="text/javascript" src="<?php echo base_url();?>js/jquery-parallax-1.1.3.js"></script>
+    <!-- jquery easing -->
+   	<script type="text/javascript" src="<?php echo base_url();?>js/jquery-easing.min.js"></script>
+   	<!-- wow -->
+   	<script type="text/javascript" src="<?php echo base_url();?>js/wow.min.js"></script>
+   	<script>
+		var wow = new WOW ({
+			boxClass: 'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset: 120,          // distance to the element when triggering the animation (default is 0)
+			mobile: false,       // trigger animations on mobile devices (default is true)
+			live: true        // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	</script> 
+	<!-- Custom Functions -->
+    <script type="text/javascript" src="<?php echo base_url();?>js/custom.js"></script>
 
 </head>

4029cd47046f9abefa0aebb8a7a1e1854870e244
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-13 21:48:50 -0500

    Modificación de información  pie de página.

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -5,24 +5,24 @@
 			<div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp animated" data-wow-duration="500ms">
 				<div class="footer-single">
 					<img src="img/footer-logo.png" alt="">
-					<p>eusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p>COPYRIGHT ©2015 thomgonzalez Prohibida su reproducción total o parcial.</p>
 				</div>
 			</div>
 		
 			<div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="300ms">
 				<div class="footer-single">
-					<h6>Subscribe </h6>
+					<h6>Suscríbete </h6>
 					<form action="#" class="subscribe">
 						<input type="text" name="subscribe" id="subscribe">
 						<input type="submit" value="&#8594;" id="subs">
 					</form>
-					<p>eusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+					<p>Suscríbete para recibir noticias.</p>
 				</div>
 			</div>
 		
 			<div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="600ms">
 				<div class="footer-single">
-					<h6>Explore</h6>
+					<h6>Explorar</h6>
 					<ul>
 						<li><a href="#">Inside Us</a></li>
 						<li><a href="#">Flickr</a></li>
@@ -34,7 +34,7 @@
 		
 			<div class="col-md-3 col-sm-6 col-xs-12 wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="900ms">
 				<div class="footer-single">
-					<h6>Support</h6>
+					<h6>Suporte</h6>
 					<ul>
 						<li><a href="#">Contact Us</a></li>
 						<li><a href="#">Market Blog</a></li>

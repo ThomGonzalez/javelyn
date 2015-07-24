bf2f8537c9c4477b4475ee7f10f5c9cb73906acf
ThomGonzalez <thom.sgonzalez@gmail.com>
2015-07-24 11:43:54 -0500

    Se quitó comentario.

--- application/views/template/base.php
+++ application/views/template/base.php
@@ -9,20 +9,15 @@
     <header id="navigation" class="navbar-fixed-top navbar">
         <div class="container">
             <div class="navbar-header">
-                <!-- responsive nav button -->
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <i class="fa fa-bars fa-2x"></i>
                 </button>
-				<!-- /responsive nav button -->
-				
-				<!-- logo -->
                 <a class="navbar-brand" href="#body">
 					<h1 id="logo">
 						<img src="<?=base_url('images/logo.png')?>" alt="ThomGonzalez">
 					</h1>
 				</a>
-				<!-- /logo -->
             </div>
 
 			<!-- main nav -->

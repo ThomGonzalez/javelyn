936d1a6ba87cfe08d3007339a20bb2382abd2db1
thomgonzalez <thom.sgonzalez@gmail.com>
2015-09-28 23:43:14 -0500

    VIEWS - Vista para mostrar datos de la consulta de la base de datos.

---
+++ application/views/consultas_view.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Consultas</title>
+</head>
+<body>
+
+<div>
+	<h1>Consultas!</h1>
+
+	<div>
+	<?php var_dump($datos) ?>
+	</div>
+</div>
+
+</body>
+</html>
